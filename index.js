1f74f37426b1e2500d49052b424c973c520ee588
BrianLevin <bml201095@gmail.com>
2021-04-13 13:15:44 -0400

    created 2nd variable whcih will generate random nuber for second dice

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ var randomImageSource =  "images/ " + randomDiceImage;
 var image1 = document.querySelectorAll('img')[0];
 
 image1.asetAtrribute("src", randomImageSource);
+
+var randomNumner2 =  Math.floor (Math.random() * 6) +1;
