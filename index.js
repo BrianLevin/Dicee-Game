5b3b179533e2f7613b60a3b6c6dc2c801dbb7219
BrianLevin <bml201095@gmail.com>
2021-04-13 13:09:29 -0400

    created a query selector to target the first image

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ var randomDiceImage =  "dice" + randomNumber1 +  ".png";
 var randomDiceImage= "dice" + randomNumber1 + ".png";
 
 var randomImageSource =  "images/ " + randomDiceImage;
+
+
+var image1 = document.querySelectorAll('img')[0];
