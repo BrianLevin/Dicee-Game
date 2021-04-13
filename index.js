c6001ba747cde803d4e38d8d1625a6ee42ff489d
BrianLevin <bml201095@gmail.com>
2021-04-13 13:21:59 -0400

    made variable which will hold the random number image for image 2

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ var image1 = document.querySelectorAll('img')[0];
 image1.asetAtrribute("src", randomImageSource);
 
 var randomNumner2 =  Math.floor (Math.random() * 6) +1;
+
+var randomImageSource2 = "images/dice" +  randomNumber2 + ".png"
