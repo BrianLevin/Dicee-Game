db11b5bb964c3fcedcb208c39e3271692623e69e
BrianLevin <bml201095@gmail.com>
2021-04-13 13:36:31 -0400

    created  a draw  scenario using document.query selector to be displayed on the page

--- index.js
+++ index.js
@@ -22,3 +22,11 @@ if (randomNumber1 > randomNumber2) {
 document.querySelector("h1").innerHTML = " 🏄‍♂️ Player 1 wins!";
 
 }
+else if (randomNumber2 > randomNumber1) {
+
+    document.querySelector("h1").innerHTML= " 🏄‍♂️ Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML= " 🏄‍♂️ Player 2 wins!";
+
+}
