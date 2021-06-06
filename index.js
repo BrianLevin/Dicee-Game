e8206b9704c9d74d8c277042f42ee4d483817a3c
BrianLevin <bml201095@gmail.com>
2021-06-06 17:47:42 -0400

    added images

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // mult random number between 1-5
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //get dice1.png - dice6.png through the images
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; // change the source or the link to get the random dice image
 
-var image1 = document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[0];  // select element start off with the first one
 
-image1.setAttribute("src", randomImageSource);
+image1.setAttribute("src", randomImageSource); // select the source and then change it to randomimage source to change dice image
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //random number
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //get random source
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);  //get second image and change the attribute
 
 
 //If player 1 wins
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!"; // change innter html  on page depending on condition
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
@@ -27,3 +27,5 @@ else if (randomNumber2 > randomNumber1) {
 else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
+
+
